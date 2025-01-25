cf278b2583952dde8ab76d3883fc0a3fa5660be2
xndroli <103154250+xndroli@users.noreply.github.com>
2025-01-25 15:11:02 -0500

    built dashboard page and header component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import Header from "@/components/header";
+import { Authenticated } from "convex/react";
+
 export default function DashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <div>{children}</div>
+        <div className="flex h-screen">
+            <Authenticated>
+                <h1>Sidebar</h1>
+                {/* <Sidebar /> */}
+            </Authenticated>
+
+            <div className="flex-1">
+                <Header />
+
+                <main>{children}</main>
+            </div>
+        </div>
     );
 };
